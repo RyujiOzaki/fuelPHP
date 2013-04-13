6e46bfd878235280b730f1fd041c965a2b2f34ce
Ryuji Ozaki <ryujiozaki@gmail.com>
2013-04-13 16:57:42 +0900

    Restの使い方

---
+++ fuel/app/classes/controller/api.php
@@ -0,0 +1,44 @@
+<?php
+
+class Controller_Api extends \Fuel\Core\Controller_Rest
+{
+    protected $format = 'json'; //コレをつけると list.jsonと書かなくても、listのみでjsonが返ってくる
+    
+    
+    public function get_list()
+    {
+        return \Model_Bb::find('all');
+    }
+   
+    
+    public function get_create()
+    {
+  
+        $bb = \Model_Bb::forge();
+        $bb->post_date = \Input::get('post_date');
+        $bb->message = \Input::get('message');
+        $bb->save();
+        
+        return array(
+            'id' => $bb->id,
+            'post_date' => $bb->post_date,
+            'message' => $bb->message,
+        );
+    }
+    //public function post_edit()
+    public function get_edit()
+    {
+        $bb = \Model_Bb::find(\Input::get('id'));
+	$bb->post_date = \Input::get('post_date');
+	$bb->message = \Input::get('message');
+	$bb->save();
+
+	return array(
+            'id' => $bb->id,
+            'post_date' => $bb->post_date,
+            'message' => $bb->message,
+            );
+	}
+}
+
+?>
